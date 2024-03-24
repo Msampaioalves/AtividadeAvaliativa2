3ed4cdcce02a41c85e0000ba13b4b63f87d2d718
Msampaioalves <164270694+Msampaioalves@users.noreply.github.com>
2024-03-24 16:19:25 +0000

    Primeiro commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner scanner = new Scanner(System.in);
+    String nomeUsuario, senha;
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    do {
+      System.out.print("Digite o nome de usuário: ");
+      nomeUsuario = scanner.nextLine();
+
+      System.out.print("Digite a senha: ");
+      senha = scanner.nextLine();
+
+      if (senha.equals(nomeUsuario)) {
+        System.out.println("Erro: A senha não pode ser igual ao nome de usuário. Tente novamente.");
+      }
+    } while (senha.equals(nomeUsuario));
+
+    System.out.println("Usuário cadastrado com sucesso!");
+  }
 }
